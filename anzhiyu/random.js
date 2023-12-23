6ddaf387d5b0d771635757f897596af45385952d
swlimuhao <limuhao@outlook.com>
2023-12-23 17:40:57 +0800

    Site updated: 2023-12-23 17:40:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/22/hello-world/","2023/12/23/Java基本数据类型与包装类型的存储方式/"];function toRandomPost(){
+var posts=["2023/12/23/Java基本数据类型与包装类型的存储方式/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
